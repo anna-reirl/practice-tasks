9bd35b7afc6dc007996233c67b626351213c4fe3
Anna <anna_agabekyan_anna@mail.ru>
2021-05-13 16:16:45 +0400

    feat: add functionality checked  and deleted

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener("click", filterTodo);
 
 
 // Functions
@@ -33,4 +36,52 @@ function addTodo(event) {
     todoDiv.appendChild(trashButton)
     //APPEND TO LIST
     todoList.appendChild(todoDiv)
+    //Clear ToDo inpurt value
+    todoInput.value = ''
 }
+
+function deleteCheck(e) {
+    // console.log(e.target)
+    const item = e.target
+    //DELETE TODO
+    if (item.classList[0] === 'trash-btn') {
+        // item.remove()
+        const todo = item.parentElement
+        // animation
+        todo.classList.add('fall')
+        // todo.remove()
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
+    }
+
+    //Check mark
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement
+        todo.classList.toggle('completed')
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+      }
+    });
+  }
